47b78c661ad8c09313ef9d678b5b5dc45a8bb661
Ivy Kabere <kabereivy@gmail.com>
2022-04-12 00:07:21 +0300

    add output propert binding to quote class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,10 +1,13 @@
 export class Quotes {
-  id!:number;
-  quote!:string;
-  author!:string;
-  userName!:string;
-  datePassed!:Date;
-  upvote!:number;
-  downvote!:number;
+  showDescription:boolean;
+  constructor(id:number,
+    quote:string,
+    author:string,
+    userName:string,
+    datePassed:Date,
+    upvote:number,
+    downvote:number){
+      this.showDescription=false;
+    }
 
 }
