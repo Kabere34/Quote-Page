3d8faf3c843e1843488c5df1bd892168f32e2b3e
Ivy Kabere <kabereivy@gmail.com>
2022-04-12 02:09:20 +0300

    add bootstrap

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,11 +7,11 @@ import { Quotes } from '../quotes';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes(1,"There is always light.If only we're brave enough to see it. If only we're brave enough to be it.","Amanda Gorman",new Date,"Daisy",0,0),
-    new Quotes(2,"I have learned not to allow rejection to move me.","Cicely Tyson",new Date,"Mark",0,0),
-    new Quotes(3, "Don't sit down and wait for the opportunities to come.Get up and make them.","Madam C.J. Walker",new Date,"Daisy",0,0),
-    new Quotes(4,"All dreams are within reach. All you have to do is keep moving towards them.","Viola Davis",new Date,"Daisy",0,0),
-    new Quotes(5,"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.","William James",new Date,"Daisy",0,0)
+    new Quotes(1,"There is always light.If only we're brave enough to see it. If only we're brave enough to be it.","Amanda Gorman",new Date(2019,3,19),"Daisy",0,0),
+    new Quotes(2,"I have learned not to allow rejection to move me.","Cicely Tyson",new Date (2019,3,19),"Mark",0,0),
+    new Quotes(3, "Don't sit down and wait for the opportunities to come.Get up and make them.","Madam C.J. Walker",new Date (2019,3,19),"Daisy",0,0),
+    new Quotes(4,"All dreams are within reach. All you have to do is keep moving towards them.","Viola Davis",new Date (2019,3,19),"Daisy",0,0),
+    new Quotes(5,"It is our attitude at the beginning of a difficult task which, more than anything else, will affect its successful outcome.","William James",new Date (2019,3,19),"Daisy",0,0)
   ];
 
 toggleDetails(index){
